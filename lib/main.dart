ad35370129e41a22e98bd128388dd8d55f8355b7
Reasondro <alessandrohasian16@gmail.com>
2024-12-09 20:16:06 +0700

    add some delay

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    initialize();
+  }
+
+  void initialize() async {
+    await Future.delayed(const Duration(milliseconds: 800));
+    FlutterNativeSplash.remove();
+  }
 
   @override
   Widget build(BuildContext context) {
